4d4be45173787a910990d6fb9da84c0cde6d3ba5
Mani Malekmohammadi <mm.mani@gmail.com>
2015-10-19 16:25:18 +0330

    revert route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 if Rails.application.config.activeadmin_associations.autocomplete_models
-  #models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
+  models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
   Rails.application.routes.prepend do
-    #get '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/, :defaults => { :format => 'json' }
-    get '/autocomplete/:model' => 'autocomplete#index', :defaults => { :format => 'json' }
+    get '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/, :defaults => { :format => 'json' }
   end
 end
