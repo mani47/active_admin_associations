0d454d62adf800d9634ea0d5cee7081d4ea040e4
Mani Malekmohammadi <mm.mani@gmail.com>
2015-10-19 13:53:43 +0330

    fix routing compatibility with rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 if Rails.application.config.activeadmin_associations.autocomplete_models
-  models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
+  #models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
   Rails.application.routes.prepend do
-    get '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/, :defaults => { :format => 'json' }
+    #get '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/, :defaults => { :format => 'json' }
+    get '/autocomplete/:model' => 'autocomplete#index', :defaults => { :format => 'json' }
   end
 end
